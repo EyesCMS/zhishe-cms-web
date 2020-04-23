30f20d275ec5a5a9c3b37cb67c90743dadd9eed6
is-lyx <60839565+is-lyx@users.noreply.github.com>
2020-04-23 21:25:55 +0800

    feat(views)add clubs page
    修改左侧边栏，添加部分页面组件

---
+++ src/views/myclub/joinclub.vue
@@ -0,0 +1,26 @@
+<template>
+<div style="margin-top: 15px;text-align:center;">
+  <el-input placeholder="请输入内容" v-model="input" class="input-with-select">
+    <el-button slot="append" icon="el-icon-search"></el-button>
+  </el-input>
+</div>
+</template>
+
+<style>
+  .el-input {
+    width: 600px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
