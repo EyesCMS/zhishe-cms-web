4734e0835c473e20439445f97577c59907210adf
is-lyx <60839565+is-lyx@users.noreply.github.com>
2020-04-28 20:02:38 +0800

    fix(club-join):add form check out

--- src/views/club-join/index.vue
+++ src/views/club-join/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h2 align="center" />
-    <el-form ref="form" :model="form" label-width="150px">
+    <el-form ref="form" :model="form" :rules="formRules" label-width="150px">
       <el-form-item label="申请人ID">
         <el-input v-model="form.userid" :disabled="true" />
       </el-form-item>
       <el-form-item label="社团名称">
         <el-input v-model="form.clubname" :disabled="true" />
       </el-form-item>
-      <el-form-item label="申请原因">
+      <el-form-item label="申请原因" prop="reason">
         <el-input v-model="form.reason" placeholder="请输入申请原因" />
       </el-form-item>
       <div style="text-align:center">
@@ -27,25 +27,33 @@ export default {
         userid: this.$store.getters.name,
         clubname: this.$route.query.name,
         reason: ''
+      },
+      formRules: {
+        reason: [
+          { required: true, message: '请输入申请原因', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
     ApplyToJoin() {
-      ApplyToJoin(this.form).then(response => {
-        if (response.status === 201) {
-          this.$alert('提交成功', '申请加入社团', {
-            confirmButtonText: '确定',
-            callback: action => {
-              this.$message({
-                type: 'info',
-                message: `提交成功！`
-              })
-            }
-          })
-        } else {
-          return this.$message.error('申请加入社团失败')
-        }
+      this.$refs.form.validate(valid => {
+        if (!valid) return
+        ApplyToJoin(this.form).then(response => {
+          if (response.status === 201) {
+            this.$alert('提交成功', '申请加入社团', {
+              confirmButtonText: '确定',
+              callback: action => {
+                this.$message({
+                  type: 'info',
+                  message: `提交成功！`
+                })
+              }
+            })
+          } else {
+            return this.$message.error('申请加入社团失败')
+          }
+        })
       })
     },
     renew() {
