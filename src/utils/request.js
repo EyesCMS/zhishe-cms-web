9f7d4b6efd4c6f8a39b498db94ddf813f1e53487
xjliang <gnulxj@gmail.com>
2020-05-08 15:31:47 +0800

    fix(expire): return to login page directly when token expired

--- src/utils/request.js
+++ src/utils/request.js
@@ -55,14 +55,9 @@ service.interceptors.response.use(
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (response.status === 401) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
+        Message.info('你未登录或者登录已过期，请重新登陆')
+        store.dispatch('user/resetToken').then(() => {
+          location.reload()
         })
       }
       return Promise.reject(new Error(res.message || 'Error'))
