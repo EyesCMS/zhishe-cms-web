ac338594a1acc461e1ba15d0aaee98e92a5661b5
xjliang <gnulxj@gmail.com>
2020-05-08 15:33:13 +0800

    chore(login & register): ui design

--- src/views/base/login/index.vue
+++ src/views/base/login/index.vue
@@ -1,84 +1,72 @@
 <template>
   <div class="login-container">
-    <section>
-      <div class="leftimg">
-        <img src="../../../assets/images/loginLeftImg.png">
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-page"
+      auto-complete="on"
+      label-position="left"
+    >
+
+      <h3 class="login-title">Zhishe 社团管理系统</h3>
+
+      <el-form-item label="Username" prop="username">
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="Username"
+          name="username"
+          type="text"
+          tabindex="1"
+          auto-complete="on"
+        />
+      </el-form-item>
+
+      <el-form-item label="Password" prop="password">
+        <el-input
+          :key="passwordType"
+          ref="password"
+          v-model="loginForm.password"
+          :type="passwordType"
+          placeholder="Password"
+          name="password"
+          tabindex="2"
+          auto-complete="on"
+          @keyup.enter.native="handleLogin"
+        />
+        <span
+          class="show-pwd"
+          @click="showPwd"
+        >
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width:100%;margin-bottom:30px;"
+        @click.native.prevent="handleLogin"
+      >Login</el-button>
+
+      <div class="tips">
+        <router-link
+          to="/forget"
+          style="float:left"
+        >
+          <span style="color:#104E8B">忘记密码?</span>
+        </router-link>
+        <!--          <span style="margin-right:20px;">username: admin</span>-->
+        <!--          <span> password: any</span>-->
+        <router-link
+          to="/register"
+          style="float:right"
+        >
+          <span style="color:#104E8B"> 注册</span>
+        </router-link>
       </div>
-      <el-form
-        ref="loginForm"
-        :model="loginForm"
-        :rules="loginRules"
-        class="login-page"
-        auto-complete="on"
-        label-position="left"
-      >
-
-        <h3 class="title">Zhishe 社团管理系统 </h3>
-        <p class="tname"> 基于 vue-element-template 的后台系统 </p>
-
-        <el-form-item prop="username">
-          <span class="svg-container">
-            <svg-icon icon-class="user" />
-          </span>
-          <el-input
-            ref="username"
-            v-model="loginForm.username"
-            placeholder="Username"
-            name="username"
-            type="text"
-            tabindex="1"
-            auto-complete="on"
-          />
-        </el-form-item>
-
-        <el-form-item prop="password">
-          <span class="svg-container">
-            <svg-icon icon-class="password" />
-          </span>
-          <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            placeholder="Password"
-            name="password"
-            tabindex="2"
-            auto-complete="on"
-            @keyup.enter.native="handleLogin"
-          />
-          <span
-            class="show-pwd"
-            @click="showPwd"
-          >
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
-        </el-form-item>
-
-        <el-button
-          :loading="loading"
-          type="primary"
-          style="width:100%;margin-bottom:30px;"
-          @click.native.prevent="handleLogin"
-        >Login</el-button>
-
-        <div class="tips">
-          <router-link
-            to="/forget"
-            style="float:left"
-          >
-            <span style="color:#104E8B">忘记密码?</span>
-          </router-link>
-          <!--          <span style="margin-right:20px;">username: admin</span>-->
-          <!--          <span> password: any</span>-->
-          <router-link
-            to="/register"
-            style="float:right"
-          >
-            <span style="color:#104E8B"> 注册</span>
-          </router-link>
-        </div>
-      </el-form>
-    </section>
+    </el-form>
   </div>
 </template>
 
@@ -156,42 +144,23 @@ export default {
 }
 </script>
 
-import bg from '@/assets/images/bg.png'
-
 <style scoped>
 .login-container {
+  min-height: 100%;
   width: 100%;
-  height: 100vh;
   overflow: hidden;
-  background-size: 100% 100%;
 }
-.leftimg {
-  width: 50%;
-  height: 100%;
-  float: left;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.login-page {
+  width: 400px;
+  margin: 150px auto;
+  border: #DCDFE6 solid 1px;
+  padding:30px 40px 20px 20px;
+  border-radius:20px;
+  box-shadow: 0 0 20px #DCDFE6;
 }
-.leftimg img {
-  width: 75%;
-  margin-left: 3rem;
-}
-.login-container section {
-  border-radius: 15px;
-  width: 50%;
-  height: 52%;
-  background: #fff;
-  border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
-  overflow: hidden;
-  box-sizing: border-box;
-  position: relative;
-  left: 25%;
-  top: 27%;
-}
-.el-input {
-  width: 92%;
+.login-title{
+  text-align: center;
+  margin-bottom:30px;
 }
 .show-pwd {
   position: absolute;
@@ -200,52 +169,5 @@ import bg from '@/assets/images/bg.png'
   font-size: 16px;
   cursor: pointer;
   user-select: none;
-}
-.login-page {
-  width: 50%;
-  float: right;
-  padding: 1rem 3rem;
-  box-sizing: border-box;
-}
-label.el-checkbox.rememberme {
-  margin: 0px 0px 15px;
-  text-align: left;
-}
-.title {
-  color: #333333;
-  font-weight: normal;
-  font-size: 1.375rem;
-  margin-bottom: 0.4rem;
-}
-.tname {
-  font-size: 0.6rem;
-  color: #a9a9a9;
-  margin: 0;
-  margin-bottom: 1.875rem;
-}
-.el-button {
-  width: 70% !important;
-  display: block;
-  border-radius: 25px;
-  margin-top: 1rem;
-  height: 2.5rem;
-}
-.el-checkbox {
-  color: #666666;
-}
-@media only screen and (min-width: 1920px) {
-  .login-page {
-    padding: 3rem;
-  }
-  .title {
-    font-size: 1.5625rem;
-  }
-  .tname {
-    margin-bottom: 4.375rem;
-  }
-  .el-button {
-    height: 3.125rem;
-    margin-top: 3rem;
-  }
 }
 </style>
--- src/views/base/register/index.vue
+++ src/views/base/register/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="content">
-    <h1>注册账号</h1>
     <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="Form">
-      <el-link type="primary" @click="back()">返回</el-link>
+      <el-link type="primary" @click="back()">使用已有账号登陆</el-link>
+      <h3 class="form-title">注册账号</h3>
       <el-form-item label="用户名" prop="username">
         <el-input v-model="ruleForm.username" />
       </el-form-item>
@@ -10,21 +10,15 @@
         <el-input v-model="ruleForm.nickname" />
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input v-model="ruleForm.password" type="password" />
+        <el-input v-model="ruleForm.password" type="password" placeholder="至少6位密码" />
       </el-form-item>
       <el-form-item label="确认密码" prop="password2">
         <el-input v-model="ruleForm.password2" type="password" />
-        <!-- <span
-          class="show-pwd"
-          @click="showPwd"
-        >
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>-->
       </el-form-item>
-      <el-form-item label="保密问题" prop="question">
+      <el-form-item label="密保问题" prop="question">
         <el-input v-model="ruleForm.question" />
       </el-form-item>
-      <el-form-item label="保密回答" prop="answer">
+      <el-form-item label="密保回答" prop="answer">
         <el-input v-model="ruleForm.answer" />
       </el-form-item>
       <el-form-item label="专业" prop="major">
@@ -168,9 +162,10 @@ export default {
 .content {
   margin: 0;
   padding: 0;
-  background-color: #2d3a4b;
+  min-height: 100%;
   width: 100%;
   height: 100%;
+  /*overflow: hidden;*/
 }
 h1 {
   margin: 0;
@@ -178,12 +173,15 @@ h1 {
   text-align: center;
   color: #eee;
 }
+.form-title{
+  text-align: center;
+  margin-bottom:30px;
+}
 .Form {
   width: 45%;
   padding: 20px;
-  margin: 0 auto;
-  border-radius: 5px;
-  background-color: aliceblue;
-  margin: 0 auto;
+  margin: 20px auto;
+  border-radius:20px;
+  box-shadow: 0 0 20px #DCDFE6;
 }
 </style>
